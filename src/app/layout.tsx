88ea8976b5a13a0d0db1f02cc6072b452d9d8eb3
deep-sutariya <d33psutariya@gmail.com>
2025-03-28 01:26:49 +0530

    update[favicon.ico]: update it to the company icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
-  title: "Company",
+  title: "Brigcom Enterprise",
   description: "Global Trade, Swiftly Made.",
 };
 
